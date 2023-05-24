58fa293d568937a51b9919e7df5ecb4ee96d5677
ChaiQ <603280349@qq.com>
2023-05-24 12:00:16 +0800

    mybatis plus API 的使用

--- src/test/java/com/course/springboot2/SpringBoot20232ApplicationTests.java
+++ src/test/java/com/course/springboot2/SpringBoot20232ApplicationTests.java
@@ -1,5 +1,6 @@
 package com.course.springboot2;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.course.springboot2.domain.User;
 import com.course.springboot2.mapper.UserMapper;
 import org.junit.jupiter.api.Test;
@@ -64,6 +65,7 @@ class SpringBoot20232ApplicationTests {
         userMapper.updateById(u);
     }
 
+    // 根据一组 id 查询
     @Test
     public void test1(){
         User u = userMapper.selectById(2);
@@ -72,9 +74,62 @@ class SpringBoot20232ApplicationTests {
         List<User> l = userMapper.selectBatchIds(idList);
     }
 
+    // 根据 id 逻辑删除
     @Test
     public void testLogicDel(){
         userMapper.deleteById(5);
+    }
+
+    // 根据条件查询数据
+    @Test
+    public void testQueryWrapper(){
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.like("name", "a")
+                .between("age", 20, 30)
+                .isNotNull("email");
+
+        // 使用 mapper 中的方法，查询数据
+        List<User> l = userMapper.selectList(queryWrapper);
+
+        l.forEach(System.out::print);
+    }
+
+    // 根据条件进行排序
+    @Test
+    public void testQueryWrapper2(){
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.orderByDesc("age").orderByAsc("id");
+
+        // 使用 mapper 中的方法，查询数据
+        List<User> l = userMapper.selectList(queryWrapper);
+
+        l.forEach(x -> System.out.println(x));
+    }
+
+    // 根据条件进行逻辑删除
+    @Test
+    public void testQueryWrapper3(){
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.isNull("email");
+
+        // 使用 mapper 中的方法，删除数据
+        userMapper.delete(queryWrapper);
+    }
+
+    // 根据条件修改数据
+    @Test
+    public void testQueryWrapper4(){
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.like("name", "a")
+                .gt("age", 20)
+                .or()
+                .isNull("email");
+
+        // 使用 mapper 中的方法，修改数据
+        User u = new User();
+        u.setAge(2);
+        u.setEmail("aaa@163.com");
+        userMapper.update(u, queryWrapper);
     }
 
 }
